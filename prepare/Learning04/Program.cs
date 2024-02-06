6f19220b0c7577096980cdcef1159d12eb618b8d
StevensMac <batescubed@gmail.com>
2024-02-06 07:09:43 -0700

    Inheritance Learning Activity FINISHED. Very cool how that works

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Console.Clear();
+        Assignment assignment = new Assignment("Jonathan Brinkerhoff","Algebra");
+        Console.WriteLine(assignment.GetSummary());
+        Console.WriteLine("");
+        MathAssignment mathAssignment = new MathAssignment("Michael Collins", "Calculus","15.9","10-28");
+        Console.WriteLine(mathAssignment.GetHomeWorkList());
+        Console.WriteLine("");
+        WritingAssignment writingAssignment = new WritingAssignment("Benjamin Phillips", "American History", "The Revolutionary War: And what came after");
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
