b09d6d03fa1c961a817f4fcb0166aebdf1662e15
StevensMac <batescubed@gmail.com>
2024-02-08 22:35:35 -0700

    Learning Activity Week 5

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapeList = new List<Shape>();
+        Square square = new Square("blue",2);
+        Rectangle rectangle = new Rectangle("red",2,3);
+        Circle circle = new Circle("purple",5);
+        shapeList.Add(square);
+        shapeList.Add(rectangle);
+        shapeList.Add(circle);
+        Console.Clear();
+        foreach (Shape shape in shapeList)
+        {
+            Console.WriteLine($"{shape} Color: {shape.GetColor()}");
+            Console.WriteLine($"{shape} Area: {shape.GetArea()}");
+            Console.WriteLine("");
+        }
     }
 }
